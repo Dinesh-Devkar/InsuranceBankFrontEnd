b948149855df35dc5d80169bf22259f7892380b4
Dinesh-Devkar <d.devkar386@gmail.com>
2022-06-18 18:29:27 +0530

    added dynamic dropdownlist for insurance types

--- src/app/policies/protection/protection.component.ts
+++ src/app/policies/protection/protection.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { DataServiceService } from 'src/app/services/data/data-service.service';
 
 @Component({
   selector: 'app-protection',
@@ -7,9 +8,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProtectionComponent implements OnInit {
 
-  constructor() { }
+  insuranceType:string=''
+  insuranceSchemes:any
+  constructor(private dataService:DataServiceService) { }
 
   ngOnInit(): void {
+    this.dataService.currentInsuranceType.subscribe((data:any)=>{
+      this.insuranceType=data
+      this.dataService.GetAllInsuranceSchemesByInsuranceType(data).subscribe((data:any)=>{
+        this.insuranceSchemes=data.$values
+        console.log(this.insuranceSchemes)
+      },(error:any)=>{
+        alert(error.error.message)
+      })
+
+    })
+    
   }
 
 }
---
+++ src/app/services/customer/customer-service.service.ts
@@ -0,0 +1,14 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CustomerServiceService {
+
+  constructor(private http:HttpClient) { }
+
+  GetCustomerDetails(){
+    return this.http.get(`http://localhost:5137/api/Customer/${sessionStorage.getItem('loggedInUser')}/GetCustomerById`)
+  }
+}
