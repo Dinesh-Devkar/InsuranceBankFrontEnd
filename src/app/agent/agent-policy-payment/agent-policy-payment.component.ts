0d7954b80034373d787d45b1bbff32f4a8cd78cc
Dinesh-Devkar <d.devkar386@gmail.com>
2022-06-18 14:02:27 +0530

    added api integration for agent and improve some admin UI

--- src/app/agent/agent-policy-payment/agent-policy-payment.component.ts
+++ src/app/agent/agent-policy-payment/agent-policy-payment.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AgentServiceService } from 'src/app/services/agent/agent-service.service';
 
 @Component({
   selector: 'app-agent-policy-payment',
@@ -7,9 +8,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AgentPolicyPaymentComponent implements OnInit {
 
-  constructor() { }
+  totalAmount:number=0
+  commissionRecordsList:any
+  constructor(private agentService:AgentServiceService) { }
 
   ngOnInit(): void {
+    this.agentService.GetCommissionRecords().subscribe((data:any)=>{
+      console.log(data)
+      this.commissionRecordsList=data.$values
+      this.CalCulaterTotalAmount()
+      
+    },(error:any)=>{
+      console.log(error)
+    })
+  }
+  CalCulaterTotalAmount(){
+    this.commissionRecordsList.forEach((insuranceAccount:any)=>{{
+      
+      this.totalAmount+=insuranceAccount.commissionAmount
+    }})
   }
 
 }
