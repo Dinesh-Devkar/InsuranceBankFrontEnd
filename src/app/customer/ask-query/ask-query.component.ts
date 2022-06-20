e472ac919548f86755432c26111a98486a142c56
Dinesh-Devkar <d.devkar386@gmail.com>
2022-06-21 00:58:17 +0530

    added post query in customer module and get queries by customerid

--- src/app/customer/ask-query/ask-query.component.ts
+++ src/app/customer/ask-query/ask-query.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup,FormControl, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { CustomerServiceService } from 'src/app/services/customer/customer-service.service';
 
 @Component({
   selector: 'app-ask-query',
@@ -7,9 +10,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AskQueryComponent implements OnInit {
 
-  constructor() { }
+  queryForm=new FormGroup({
+    customerId:new FormControl(''),
+    title:new FormControl('',Validators.required),
+    message:new FormControl('',Validators.required)
+  })
+  constructor(private customerService:CustomerServiceService,private router:Router) { }
 
+  PostQuery(){
+    this.customerService.PostQuery(this.queryForm.value).subscribe((data:any)=>{
+      alert(data.message)
+      this.router.navigate(['/customerdashboard'])
+    },(error:any)=>{
+      console.log(error)
+      alert(error.message)
+    })
+  }
   ngOnInit(): void {
+    this.queryForm.get('customerId')?.setValue(sessionStorage.getItem('loggedInUser'))
   }
 
 }
