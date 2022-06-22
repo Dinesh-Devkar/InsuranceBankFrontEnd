88ed1bead0d2fc512af6c73632d3bfbcf1ba6cbf
Dinesh-Devkar <d.devkar386@gmail.com>
2022-06-22 14:12:16 +0530

    added edit insurance scheme in admin module

--- src/app/shared/customer-roll-guard.guard.ts
+++ src/app/shared/customer-roll-guard.guard.ts
@@ -1,15 +1,19 @@
 import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
 import { Observable } from 'rxjs';
+import { AuthServiceService } from '../services/auth-service.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CustomerRollGuardGuard implements CanActivate {
-  canActivate(
-    route: ActivatedRouteSnapshot,
-    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-    return true;
+  constructor(private authService:AuthServiceService,private router:Router){}
+  canActivate(){
+    if(this.authService.HaveAccess()=="Customer"){
+      return true;
+    }
+    this.router.navigate(['/'])
+    return false;
   }
   
 }
