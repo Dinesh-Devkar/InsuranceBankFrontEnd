0364346a1cc0cc324d11564cefe9ca154390f9bb
Dinesh-Devkar <d.devkar386@gmail.com>
2022-06-23 19:56:31 +0530

    added edit profile for agent module

--- src/app/services/agent/agent-service.service.ts
+++ src/app/services/agent/agent-service.service.ts
@@ -29,8 +29,11 @@ export class AgentServiceService {
   GetCommissionRecords(){
     return this.http.get(`http://localhost:5137/api/Agent/${sessionStorage.getItem('loggedInUser')}/GetCommissionRecordsByAgentId`)
   }
-  UpdateAgent(agentCode:any,agent:any){
-    return this.http.put(`http://localhost:5137/api/v1/Admin/${agentCode}/UpdateAgent`,agent)
+  UpdateAgent(agentId:any,agent:any){
+    return this.http.put(`http://localhost:5137/api/agent/${agentId}/UpdateAgent`,agent)
+  }
+  GetAgentDetailsByAgentId(agentId:any) {
+    return this.http.get(`http://localhost:5137/api/Agent/${agentId}/GetAgentById`)
   }
 
   
